fff6e24c88c009426f351f71a53e8deae3504609
largado <alexandrohipolito@gmailcom>
2022-01-29 12:25:18 -0300

    bolinha pronta

--- 1.js
+++ 1.js
@@ -1,8 +1,12 @@
+//variáveis da bolinha
 let xBolinha = 300;
 let yBolinha = 200;
 let diametro22 = 15;
+let raio = diametro22 / 2;
 
-let velocidadexdaBolinha = 6;
+//velocidade da bolinha
+
+let velocidadexdaBolinha = 16;
 let velocidadeydaBolinha = 6;
 
 function setup() {
@@ -11,11 +15,30 @@ function setup() {
 
 function draw() {
   background(20);
+  mostraBolinha();
+  movimentaBolinha();
+  verificaSeColide();
+
+}
+
+function mostraBolinha(){
   circle(xBolinha, yBolinha, diametro22);
-  xBolinha += velocidadexdaBolinha;
-  //yBolinha =+ yBolinha + velocidadeydaBolinha;
+}
+
+function movimentaBolinha(){
   
-  if(xBolinha > width || xBolinha < 0) {
+  xBolinha += velocidadexdaBolinha;
+  yBolinha =+ yBolinha + velocidadeydaBolinha;
+  
+}
+
+function verificaSeColide(){
+    if(xBolinha + raio > width || xBolinha - raio < 0) {
     velocidadexdaBolinha *= -1;
+    
   }
+  if(yBolinha + raio > height || yBolinha - raio <0){
+    velocidadeydaBolinha *= -1;
+  }
+  
 }
